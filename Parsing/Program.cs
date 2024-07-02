92b50488ac0736444062befabbe8d3b632739fb2
Gustav Berg <Gustav.Berg@xlent.se>
2024-07-02 14:49:12 +0200

    Parse from string complete

--- Parsing/Program.cs
+++ Parsing/Program.cs
@@ -4,54 +4,46 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            string numberString = "42";
-            if (int.TryParse(numberString, out int number))
-            {
-                Console.WriteLine($"Parsed integer: {number}");
-            }
-            else
-            {
-                Console.WriteLine("Failed to parse integer");
-            }
+            #region Parse From String
 
-            string floatString = "3.14";
-            if (float.TryParse(floatString, out float floatNumber))
-            {
-                Console.WriteLine($"Parsed float: {floatNumber}");
-            }
-            else
-            {
-                Console.WriteLine("Failed to parse float");
-            }
+            string age = "23";
+            Console.WriteLine($"{age}. Is of type: {age.GetType().Name}\n");
 
-            string dateString = "2024-07-02";
-            if (DateTime.TryParse(dateString, out DateTime date))
-            {
-                Console.WriteLine($"Parsed date: {date.ToShortDateString()}");
-            }
-            else
-            {
-                Console.WriteLine("Failed to parse date");
-            }
+            int ageAsNumber = int.Parse(age);
+            Console.WriteLine($"{ageAsNumber}. Is of type: {ageAsNumber.GetType().Name}\n");
 
-            string boolString = "true";
-            if (bool.TryParse(boolString, out bool boolValue))
-            {
-                Console.WriteLine($"Parsed boolean: {boolValue}");
-            }
-            else
-            {
-                Console.WriteLine("Failed to parse boolean");
-            }
+            string ageError = "23a";
+            //int ageAsNumberError = int.Parse(ageError);
+            //Console.WriteLine(ageAsNumberError);
 
-            string csvString = "apple,banana,cherry";
-            string[] fruits = csvString.Split(',');
-            Console.WriteLine("Parsed fruits:");
-            foreach (string fruit in fruits)
-            {
-                Console.WriteLine(fruit);
-            }
+            decimal ageAsDecimal = decimal.Parse(age);
+            Console.WriteLine($"{ageAsDecimal}. Is of type: {ageAsDecimal.GetType().Name}\n");
+
+            string singleLetter = "A";
+            Console.WriteLine($"{singleLetter}. Is of type: {singleLetter.GetType().Name}\n");
+
+            char letter = char.Parse(singleLetter);
+            Console.WriteLine($"{letter}. Is of type: {letter.GetType().Name}\n");
+
+            string guidAsString = "67ad96d0-f367-4cc2-a8c8-c37533317cde";
+            Guid guid = Guid.Parse(guidAsString);
+            Console.WriteLine($"{guid}. Is of type: {guid.GetType().Name}\n");
+
+
+            string boolAsString = "true";
+            bool boolFromString = bool.Parse(boolAsString);
+            Console.WriteLine($"{boolFromString}. Is of type: {boolFromString.GetType().Name}\n");
+
+            string decimalAsString = "1,2";
+            decimal decimalFromString = decimal.Parse(decimalAsString);
+            Console.WriteLine($"{decimalFromString}. Is of type: {decimalFromString.GetType().Name}\n");
+
+            #endregion
+
+
+
+
+
         }
     }
 }
